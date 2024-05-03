9d8b306a4185c0b0f6ce511d59f0ef056b8c2b6a
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-05-03 15:45:08 +0900

    Answer Entity

---
+++ src/main/java/com/sbs/sbb/Answer.java
@@ -0,0 +1,27 @@
+package com.sbs.sbb;
+
+import jakarta.persistence.*;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity // anser 테이블
+public class Answer {
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    // private Integer questionId
+    @ManyToOne
+    private Question question;
+}
+
