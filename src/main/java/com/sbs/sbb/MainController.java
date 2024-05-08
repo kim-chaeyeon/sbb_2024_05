67d28e8e98ca3283deab4252d9d4cf47eb1a5815
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-05-08 15:45:25 +0900

    root url 리다이렉트

--- src/main/java/com/sbs/sbb/MainController.java
+++ src/main/java/com/sbs/sbb/MainController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-
     @GetMapping("/sbb")
     @ResponseBody
     public String index() {
-        return "안녕하세요!!!!!!!";
+        return "안녕하세요!!!";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
     }
 }
