fee97546d251f7738b3443d9b45b539939986b7f
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-05-08 15:26:52 +0900

    데이터 각각 레너렌더링

--- src/main/java/com/sbs/sbb/question/Question.java
+++ src/main/java/com/sbs/sbb/question/Question.java
@@ -4,29 +4,32 @@ import com.sbs.sbb.answer.Answer;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
-@Entity // Question 테이블
+@Entity // question 테이블
+@ToString
 public class Question {
-    @Id // primary key
+    @Id // Primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
     private Integer id;
 
-    @Column(length = 200) // varchar(200)
+    @Column(length = 200) // VARCHAR(200)
     private String subject;
 
-    @Column(columnDefinition = "TEXT") // text
+    @Column(columnDefinition = "TEXT") // TEXT
     private String content;
 
     private LocalDateTime createDate;
 
     // mappedBy Answer 클래스의 question 변수 이름을 적어야 함.
     // CascadeType.REMOVE를 하면 Question을 삭제할 때 답변도 함께 삭제됨.
-    // OneToMany는 테이블의 커럼으로 생성되지 않음.
+    // OneToMany는 테이블의 컬럼으로 생성되지 않음.
+    // 선택
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
 }
