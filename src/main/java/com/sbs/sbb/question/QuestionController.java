d6798cbb69e8eef0fa31aa1b88304fbf3f2e2c0d
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-05-09 17:19:50 +0900

    질문 등록 기능

--- src/main/java/com/sbs/sbb/question/QuestionController.java
+++ src/main/java/com/sbs/sbb/question/QuestionController.java
@@ -3,9 +3,7 @@ package com.sbs.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,7 +31,14 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String questionCreate() {
+    public String create() {
         return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
+        Question q = this.questionService.create(subject, content);
+
+        return "redirect:/question/list";
     }
 }
