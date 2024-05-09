a1a7b3d915527393b1ae049c7323cae2dd2b301f
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-05-09 16:43:19 +0900

    질문 등록 폼

--- src/main/java/com/sbs/sbb/question/QuestionController.java
+++ src/main/java/com/sbs/sbb/question/QuestionController.java
@@ -31,4 +31,9 @@ public class QuestionController {
 
         return "question_detail";
     }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
 }
