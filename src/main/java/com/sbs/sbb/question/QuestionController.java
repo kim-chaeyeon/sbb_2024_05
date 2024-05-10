8ead43c3f999f375a4b2191f89a2a7af2fd5304f
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-05-10 16:17:35 +0900

    답변 등록에 폼 유효성 체크 적용

--- src/main/java/com/sbs/sbb/question/QuestionController.java
+++ src/main/java/com/sbs/sbb/question/QuestionController.java
@@ -1,5 +1,6 @@
 package com.sbs.sbb.question;
 
+import com.sbs.sbb.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -28,7 +29,7 @@ public class QuestionController {
     }
 
     @GetMapping("/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question q = this.questionService.getQuestion(id);
 
         model.addAttribute("question", q);
